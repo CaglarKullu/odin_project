178858095225a9e221e0be7d8b89669c05b4bcc1
Caglar Kullu <caglarkullu@gmail.com>
2024-03-03 16:47:52 +0000

    add: add ilbrary class

--- library/script.js
+++ library/script.js
@@ -8,15 +8,28 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.read = read;
-    }   
-
-    addBookToLibrary() {
-        // Ensure myLibrary is an array before pushing new Book
-        if (!Array.isArray(myLibrary)) {
-            console.error('myLibrary is not an array.');
-            return;
-        }
-        myLibrary.push(this);
     }
+
+    // This static method is no longer needed as we can directly use the constructor
+};
+
+// Refactor the addBookToLibrary method to be a static method that takes a book instance
+Book.addBookToLibrary = function(book) {
+    if (!Array.isArray(myLibrary)) {
+        console.error('myLibrary is not an array.');
+        return;
+    }
+    myLibrary.push(book);
+};
+
+// This static method is no longer needed as we can directly use the constructor
+Book.newBook = function(title, author, pages, read) {
+    return new Book(title, author, pages, read);
 }
+
+// function to add a book to the library
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = Book.newBook(title, author, pages, read);
+    newBook.addBookToLibrary();
+};
 
