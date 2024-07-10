821069ebb24eacb93909833d58cb2ac95816e451
Caglar Kullu <caglarkullu@gmail.com>
2024-07-10 16:08:30 +0100

    first commit

---
+++ weather_app/app.js
@@ -0,0 +1,40 @@
+document.getElementById('weatherForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+    getWeather(city);
+});
+
+function getWeather(city) {
+    const apiKey = '300c83b131452629669fa87274305e27';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => displayWeather(data))
+        .catch(error => console.error('Error fetching the weather data:', error));
+
+    // print data from api
+   function printData(apiUrl) {
+       fetch(apiUrl)
+           .then(response => response.json())
+           .then(data => console.log(data))
+           .catch(error => console.error('Error fetching the weather data:', error));
+   };
+
+   printData(apiUrl);
+}
+
+function displayWeather(data) {
+    if (data.cod === 200) {
+        const weatherDiv = document.getElementById('weatherResult');
+        weatherDiv.innerHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+        `;
+    } else {
+        alert('City not found. Please try again.');
+    }
+}
