e324d91fac53df355722290e5ca2082186f2a137
Caglar Kullu <caglarkullu@gmail.com>
2024-07-12 11:05:41 +0100

    clean up

--- React_apps/cv_app/cv-app/src/App.jsx
+++ React_apps/cv_app/cv-app/src/App.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
-import Form from "./components/Form/Form";
-import Preview from "./components/preview/Preview";
-import DownloadButton from "./components/DownloadButton";
 import "./App.css";
 
 function App() {
-  const [cvData, setCvData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    education: "",
-    experience: ""
-  });
+
 
   return (
     <div className="App">
-      <h1>CV Creator</h1>
-      <div className="container">
-        <Form cvData={cvData} setCvData={setCvData} />
-        <Preview cvData={cvData} />
-      </div>
-      <DownloadButton cvData={cvData} />
+
     </div>
   );
 }
