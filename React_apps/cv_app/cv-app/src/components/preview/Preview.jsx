1c0d0ed5a837c4ac2b1f2629a5d93cdfb023eb84
Caglar Kullu <caglarkullu@gmail.com>
2024-07-24 16:15:46 +0100

    feature: color selector skelton added

--- React_apps/cv_app/cv-app/src/components/preview/Preview.jsx
+++ React_apps/cv_app/cv-app/src/components/preview/Preview.jsx
@@ -5,18 +5,21 @@ import TemplateOne from './template_one/TemplateOne';
 import TemplateTwo from './template_two/TemplateTwo';
 import TemplateThree from './template_three/TemplateThree';
 import './Preview.css';
+import ColorSelector from './color_selector/ColorSelector';
 
 const Preview = () => {
   const { formData } = useFormContext();
   const [template, setTemplate] = useState('template1');
+  const [color, setColor] = useState('#1e90ff');
 
   return (
     <div id="Preview">
       <TemplateSelector setTemplate={setTemplate} />
+      <ColorSelector />
       <div className="preview-container" id="preview-content">
         {template === 'template1' && <TemplateOne formData={formData} />}
         {template === 'template2' && <TemplateTwo formData={formData} />}
-        {template === 'template3' && <TemplateThree formData={formData} />}
+        {template === 'template3' && <TemplateThree formData={formData}  color={color}/>}
       </div>
     </div>
   );
